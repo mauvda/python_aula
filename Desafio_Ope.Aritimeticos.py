52d042eb31f8960685e6263b15bb3db42f841db0
mauriciolamonatto <mauricio.lamonatto@gmail.com>
2024-06-11 10:25:49 -0300

    Adicionando primeiro programa criado em python em sua primeira versão

---
+++ Desafio_Ope.Aritimeticos.py
@@ -0,0 +1,11 @@
+# Variaveis
+salario = 3450.45
+despesas = 2456.2
+percentual = 100-((despesas*100) // salario)
+
+# Percentual de gastos
+print(f"Se seu salario e {salario}" "\n"
+      f"E seus gastos chegam a {despesas}" "\n"
+      f"O percentual do seu salario "
+      f"restante e {int(percentual)}%")
+# print((despesas * 100)//salario)
---
+++ fundamentos_projetos/area_circulo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from math import pi
+
+raio = 15.3
+
+print("Area do circulo é: ", pi * raio ** 2)
