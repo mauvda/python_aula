8fa76589a829c2296770b55ec30f7c217542d35e
mauriciolamonatto <mauricio.lamonatto@gmail.com>
2024-06-11 21:03:35 -0300

    Versao final do primeiro programa em python

--- fundamentos_projetos/area_circulo.py
+++ fundamentos_projetos/area_circulo.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
-
 from math import pi
+import sys
+import errno
 
-raio = 15.3
 
-print("Area do circulo é: ", pi * raio ** 2)
+class TerminalColor:
+    ERRO = '\033[91m'
+    NORMAL = '\033[0m'
+
+
+def help():
+    print(TerminalColor.ERRO +
+          "Inclua um valor numerico no comando \n"
+          "utilize ", sys.argv[0][2:], " <raio>" +
+          TerminalColor.NORMAL)
+
+
+def circulo(raio):
+    return pi * raio ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+        sys.exit(errno.EPERM)
+
+    elif not sys.argv[1].isnumeric():
+        help()
+        sys.exit(errno.EINVAL)
+
+    raio = float(sys.argv[1])
+    print("Area do circulo é: ", circulo(raio))
+else:
+    print("você está no módulo ", __name__)
