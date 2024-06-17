1ae883189727f082f97021649e6a1ea67405d5ab
mauriciolamonatto <mauricio.lamonatto@gmail.com>
2024-06-17 08:17:16 -0300

    ajuste de diretorios

--- exemplos/estrutura_repeticao.py
+++ exemplos/estrutura_repeticao.py
@@ -1,9 +1,9 @@
 from random import randint
 
-numero_informado = {20}
-numero_secreto = int (randint(0, 9))
+numero_informado = 20
+numero_secreto = int(randint(0, 9))
 
-while numero_secreto not in numero_informado:
+while numero_informado != numero_secreto:
     numero_informado = int(randint(0, 9))
     if numero_informado != numero_secreto:
         print('Voce tentou o numero', numero_informado, 'e falhou')
